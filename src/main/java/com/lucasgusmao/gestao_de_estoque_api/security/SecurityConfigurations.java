b65900f09551dce5c68d51fabd9f5f25b17d3b00
Lucas Gusmão <130295673+lucasgusmaobaptista@users.noreply.github.com>
2025-03-06 12:32:03 -0300

    Update SecurityConfigurations.java

--- src/main/java/com/lucasgusmao/gestao_de_estoque_api/security/SecurityConfigurations.java
+++ src/main/java/com/lucasgusmao/gestao_de_estoque_api/security/SecurityConfigurations.java
@@ -32,7 +32,7 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers("api/produtos/**", "api/fornecedores/**", "api/categorias/**").hasRole("ADMIN")
-                        .anyRequest().permitAll())
+                        .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
